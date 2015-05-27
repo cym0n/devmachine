37b0ee370b3adb0ee96f2a06d817b9ab44715fcc
cym0n <cymon@tiscali.it>
2015-05-27 21:24:14 +0200

    Site installer sql has now a version that ask for password

--- scripts/siteInstaller-SQL-pwd.sh
+++ scripts/siteInstaller-SQL-pwd.sh
@@ -1,11 +1,11 @@
 PROJECT=$1
 echo "CREATE USER '$PROJECT'@'%' IDENTIFIED BY '$PROJECT'" > query.sql
-mysql -u root < query.sql
+mysql -u root -p < query.sql
 echo "CREATE USER '$PROJECT'@'localhost' IDENTIFIED BY '$PROJECT'" > query.sql
-mysql -u root < query.sql
+mysql -u root -p  < query.sql
 echo "CREATE DATABASE $PROJECT" > query.sql
-mysql -u root < query.sql
+mysql -u root -p < query.sql
 echo "GRANT ALL ON $PROJECT.* TO $PROJECT; FLUSH PRIVILEGES" > query.sql
-mysql -u root < query.sql
+mysql -u root -p < query.sql
 rm query.sql
 
